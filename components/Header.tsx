f34e378d7e17452ee11b86afbcac920cfc81ec1e
Vikash Thakur <thakvikash622@gmail.com>
2024-11-25 18:49:33 +0530

    Created: Header

---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+import { ShoppingBag } from 'lucide-react'
+import React from 'react'
+import { Button } from './ui/button'
+
+const menu = [
+    {
+        name: "Home"
+    },
+    {
+        name: "Store"
+    },
+    {
+        name: "Explore"
+    },
+]
+
+const Header = () => {
+    return (
+        <section className='bg-gray-300 flex items-center justify-between px-10 py-5'>
+            <h2 className='font-bold text-3xl cursor-pointer hover:scale-105 transition-all'>E-Store</h2>
+            <ul className='flex items-center justify-center gap-5 cursor-pointer'>
+                {menu.map((item, index) => (
+                    <li key={index} className='font-bold text-xl hover:bg-gray-400 p-2 rounded-2xl'>
+                        {item.name}
+                    </li>
+                ))}
+            </ul>
+            <div className='flex items-center gap-5'>
+                <ShoppingBag className='size-8' />
+                <Button>
+                    Click Here
+                </Button>
+            </div>
+        </section>
+    )
+}
+
+export default Header
