a163659d098f2d1384bc64b5fba0c6e0d0c3b69e
Vikash Thakur <thakvikash622@gmail.com>
2024-11-26 00:27:34 +0530

    Fixed: Responsiveness

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -12,12 +12,12 @@ const ProductList = () => {
         setProducts(dummyData)
     }, [])
     return (
-        <div className='px-32 my-5'>
+        <div className='px-4 md:px-32 my-5'>
             <div className='flex justify-between items-center'>
                 <h1 className='font-bold text-xl'>Feature</h1>
                 <Button>View All</Button>
             </div>
-            <div className='mt-5 grid grid-cols-1 md:grid-cols-3 gap-10'>
+            <div className='mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {products.map((product) => (
                     <ProductCard product={product} key={product.id} />
                 ))}
