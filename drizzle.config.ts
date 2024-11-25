50a1a7b7467e3c21da2390c1c15c7b722e40d7b1
Vikash Thakur <thakvikash622@gmail.com>
2024-11-26 00:13:59 +0530

    Minor changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
   schema: './config/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
