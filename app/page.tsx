42cd3952cdba4bfc76ffde545400001b1b31a64f
Vikash Thakur <thakvikash622@gmail.com>
2024-11-25 19:08:27 +0530

    Fixed: Basic landing UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      welcome to the home page
+    <div className='bg-green-500'>
+      <div className='grid grid-cols-1 md:grid-cols-2 px-5 py-5 md:px-32 md:py-20'>
+        <div className='flex flex-col items-start justify-center gap-5'>
+          <h1 className='font-bold text-3xl md:text-5xl'>SpeedUp Your Creative WorkFlow</h1>
+          <p className='font-semibold text-gray-600 md:text-xl'>Join a growing family of 43,436 designers, creator and makers from around
+          the world</p>
+          <div className='flex gap-5 w-full'>
+            <Button>Explore</Button>
+            <Button>Sell</Button>
+          </div>
+        </div>
+        <div>
+          <Image src={"/pc.png"} alt='Landing Image' width={500} height={60} />
+        </div>
+      </div>
     </div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -16,9 +16,9 @@ const menu = [
 
 const Header = () => {
     return (
-        <section className='bg-gray-300 flex items-center justify-between px-10 py-5'>
-            <h2 className='font-bold text-3xl cursor-pointer hover:scale-105 transition-all'>E-Store</h2>
-            <ul className='flex items-center justify-center gap-5 cursor-pointer'>
+        <section className='bg-gray-300 flex items-center justify-between px-5 md:px-10 py-5'>
+            <h2 className='font-bold text-xl md:text-3xl cursor-pointer hover:scale-105 transition-all'>E-Store</h2>
+            <ul className='flex items-center justify-center gap-5 cursor-pointer max-sm:hidden'>
                 {menu.map((item, index) => (
                     <li key={index} className='font-bold text-xl hover:bg-gray-400 p-2 rounded-2xl'>
                         {item.name}
